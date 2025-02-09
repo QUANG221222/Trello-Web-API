2f43444d3bfd796af0fba0e3a92e5a353d9b88c0
QUANG221222 <nguyennhatquang.2509@gmail.com>
2025-02-09 16:42:25 +0700

    Mongodb Aggregate

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -2,12 +2,18 @@ import Joi from 'joi'
 import { GET_DB } from '~/config/mongodb'
 import { ObjectId } from 'mongodb'
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
+import { BOARD_TYPES } from '~/utils/constants'
+import { columnModel } from '~/models/columnModel'
+import { cardModel } from '~/models/cardModel'
+
 //Define Colection (Name & Schema)
 const BOARD_COLLECTION_NAME = 'boards'
 const BOARD_COLLECTION_SCHEMA = Joi.object({
   title: Joi.string().required().min(3).max(50).trim().strict(),
   slug: Joi.string().required().min(3).trim().strict(),
   description: Joi.string().required().min(3).max(256).trim().strict(),
+
+  type: Joi.string().valid(BOARD_TYPES.PUBLIC, BOARD_TYPES.PRIVATE).required(),
 
   // Lưu ý các item trong mảng columnOrderIds là ObjectId nên cần thêm pattern cho chuẩn nhé, (lúc quay video số 57 mình quên nhưng sang đầu video số 58 sẽ có nhắc lại về cái này.)
   columnOrderIds: Joi.array()
@@ -53,10 +59,37 @@ const findOneById = async (id) => {
 //Query tổng hợp (aggregate) để lấy toàn bộ Column và Cards thuộc về Board
 const getDetails = async (id) => {
   try {
+    // const result = await GET_DB()
+    //   .collection(BOARD_COLLECTION_NAME)
+    //   .findOne({ _id: new ObjectId(id) })
     const result = await GET_DB()
       .collection(BOARD_COLLECTION_NAME)
-      .findOne({ _id: new ObjectId(id) })
-    return result
+      .aggregate([
+        {
+          $match: {
+            _id: new ObjectId(id),
+            _destroy: false
+          }
+        },
+        {
+          $lookup: {
+            from: columnModel.COLUMN_COLLECTION_NAME,
+            localField: '_id',
+            foreignField: 'boardId',
+            as: 'columns'
+          }
+        },
+        {
+          $lookup: {
+            from: cardModel.CARD_COLLECTION_NAME,
+            localField: '_id',
+            foreignField: 'boardId',
+            as: 'cards'
+          }
+        }
+      ])
+      .toArray()
+    return result[0] || {}
   } catch (error) {
     throw new Error(error)
   }
@@ -69,3 +102,6 @@ export const boardModel = {
   findOneById,
   getDetails
 }
+// boardID : 67a86a4bd6388a26c2404a8b
+// columnID: 67a86fef2694be6164109cde
+// cardID: 67a871692694be6164109ce1
